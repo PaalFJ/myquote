fa75cbd28ee83e49cfd1e0dd4c4c81adbeb064f8
paalf <paalfolmer@gmail.com>
2022-01-27 14:18:37 +0100

    ny commit

---
+++ myquote.go
@@ -0,0 +1,21 @@
+package myquote
+    import (
+    "fmt"
+    "rsc.io/quote"
+    )
+
+    func hello() {
+    fmt.Println(quote.Hello())
+    }
+
+    func opt(){
+    fmt.Println(quote.Opt)
+    }
+
+    func glass(){
+    fmt.Println(quote.Glass)
+    }
+
+    func go(){
+        fmt.Println(quote.Go)
+        }
